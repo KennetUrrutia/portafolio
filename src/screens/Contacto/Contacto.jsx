73325b7080acf4045d28f78a05d6ed90ba7ab6eb
Kenneth De Leon <Kenneth@somosmangoverde.com>
2023-01-20 20:07:43 -0600

    inicio de formulario de contacto

--- src/screens/Contacto/Contacto.jsx
+++ src/screens/Contacto/Contacto.jsx
@@ -1,10 +1,16 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
+import styles from "./styles"
 
 export const Contacto = () => {
   return (
-    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#223039', height: '100vh', width: '100vw'}}>
-      <Box>
-
+    <Box sx={styles.container}>
+      <Box sx={styles.contactContainer}>
+        <Box sx={styles.titleContainer}>
+          <Typography
+            variant="h1"
+            sx={styles.title}
+          >Formulario de contacto</Typography>
+        </Box>
       </Box>
     </Box>
   )
---
+++ src/screens/Contacto/styles.js
@@ -0,0 +1,41 @@
+const container = {
+  display: 'flex', 
+  justifyContent: 'center', 
+  alignItems: 'center', 
+  backgroundColor: '#223039', 
+  height: '100vh', 
+  width: '100vw'
+}
+
+const contactContainer = {
+  width: '70vw', 
+  height: '75vh', 
+  backgroundColor: 'white', 
+  borderRadius: '10px'
+}
+
+const titleContainer = {
+  height: '15%', 
+  width: '100%', 
+  backgroundColor: '#f8af75', 
+  borderRadius: '10px 10px 0 0', 
+  display: 'flex', 
+  justifyContent: 'center', 
+  alignItems: 'center'
+}
+
+const title = {
+  fontFamily: 'Poppins',
+  fontWeight: 'bold',
+  fontSize: '50px',
+  color: 'white'
+}
+
+const styles = {
+  container,
+  contactContainer,
+  titleContainer,
+  title,
+}
+
+export default styles
